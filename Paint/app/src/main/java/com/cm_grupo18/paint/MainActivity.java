d1b309f6d17a8da9fc7abece713e79884b549469
Daniel Bento <danielbento_98@hotmail.com>
2020-10-02 10:54:04 +0100

    adicionado primeiro objetivo de CM cumprido

---
+++ Paint/app/src/main/java/com/cm_grupo18/paint/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cm_grupo18.paint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static int TIME_OUT = 3000; //Time to launch the another activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, PaintActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
---
+++ Paint/app/src/main/java/com/cm_grupo18/paint/PaintActivity.java
@@ -0,0 +1,48 @@
+package com.cm_grupo18.paint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PaintActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static int background = Color.argb(255, 255, 255, 255);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+
+        Button settingsBtn = findViewById(R.id.settings_button);
+        settingsBtn.setOnClickListener(this);
+
+        Button aboutBtn = findViewById(R.id.about_button);
+        aboutBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        View view = getWindow().getDecorView();
+        view.setBackgroundColor(background);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.settings_button:
+                startActivity(new Intent(PaintActivity.this, SettingsActivity.class));
+                break;
+            case R.id.about_button:
+                startActivity(new Intent(PaintActivity.this, AboutActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
+
+}
