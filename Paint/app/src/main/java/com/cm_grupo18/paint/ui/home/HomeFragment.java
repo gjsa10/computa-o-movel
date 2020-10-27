72281eec7bfdecb6c10bc522888183ac73956df0
daniel <danielbento_98@hotmail.com>
2020-10-27 17:12:41 +0000

    Added 2 fragments for paint and pallete used in home

--- Paint/app/src/main/java/com/cm_grupo18/paint/ui/home/HomeFragment.java
+++ Paint/app/src/main/java/com/cm_grupo18/paint/ui/home/HomeFragment.java
@@ -16,20 +16,9 @@ import com.cm_grupo18.paint.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
---
+++ Paint/app/src/main/java/com/cm_grupo18/paint/ui/home/PaintFragment.java
@@ -0,0 +1,30 @@
+package com.cm_grupo18.paint.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cm_grupo18.paint.R;
+
+public class PaintFragment extends Fragment {
+
+    public PaintFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_paint, container, false);
+    }
+}
